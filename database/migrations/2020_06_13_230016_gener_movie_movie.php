b9d0af5b7295e04e0824c2d44bafed84a74b0569
YoSoyAdrian <52388238+YoSoyAdrian@users.noreply.github.com>
2020-06-14 13:36:48 -0600

    Migraciones corregidas

--- database/migrations/2020_06_13_230016_gener_movie_movie.php
+++ database/migrations/2020_06_13_230016_gener_movie_movie.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMoviesTable extends Migration
+class GenerMovieMovie extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateMoviesTable extends Migration
      */
     public function up()
     {
-        Schema::create('movies', function (Blueprint $table) {
+        //
+        Schema::create('gener_movie_movie', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->text('sysnopsis');
-            $table->boolean('active')->default(true);
-            $table->unsignedInteger('classification_movie_id');
+            $table->unsignedInteger('gener_movie_id');
+            $table->unsignedInteger('movie_id');
             $table->timestamps();
-            $table->foreign('classification_movie_id')->references('id')->on('classification_movies');
+            $table->foreign('gener_movie_id')->references('id')->on('gener_movies');
+            $table->foreign('movie_id')->references('id')->on('movies');
         });
     }
 
@@ -31,6 +31,6 @@ class CreateMoviesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('movies');
+        //
     }
 }
